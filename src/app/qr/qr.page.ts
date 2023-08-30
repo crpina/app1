1724fdd776cb0bd36577fc1c42b300170f99c0db
Esteban Tamayo Chamorro <es.tamayo@duocuc.cl>
2023-08-30 00:04:18 -0400

    Primera parte de codigo QR
    Se agrega la funcionalidad de activar la camara para escanear codigo QR, a su vez  se agrega un codigo qr donde se agregara la informacion del usuario.

--- src/app/qr/qr.page.ts
+++ src/app/qr/qr.page.ts
@@ -1,15 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BarcodeScanner} from "@capacitor-community/barcode-scanner";
 
 @Component({
   selector: 'app-qr',
   templateUrl: './qr.page.html',
   styleUrls: ['./qr.page.scss'],
 })
-export class QrPage implements OnInit {
+export class QrPage implements OnDestroy {
 
-  constructor() { }
 
-  ngOnInit() {
+  qrCodeString = 'This is a secret qr code message';
+  scannedResult: any;
+  content_visibility = '';
+
+  constructor(
+
+  ) {}
+
+  async checkPermission() {
+    try {
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        return true;
+      }
+      return false;
+    } catch(e) {
+      console.log(e);
+    } return false;
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if(!permission) {
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      document.querySelector('body')?.classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.querySelector('body')?.classList.remove('scanner-active');
+      this.content_visibility = '';
+      if(result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    } catch(e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body')?.classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
   }
 
 }
