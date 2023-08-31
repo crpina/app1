a04e87d2aecf77c03b6ff6ff72678248e488e675
Cristopher Enrique Piña Lillo <criss6121topher@gmail.com>
2023-08-31 11:24:15 -0400

    cambios android

--- android/app/src/main/java/com/fazweb/app/MainActivity.java
+++ android/app/src/main/java/com/fazweb/app/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.fazweb.app;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AppMobil',
+  appId: 'com.fazweb.app',
+  appName: 'appmobil',
   webDir: 'www',
   server: {
     androidScheme: 'https'
